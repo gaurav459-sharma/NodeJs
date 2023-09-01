23db90f05448d8b12959a3182355c86176337197
gaurav459-sharma <sharmag32145@gmail.comm>
2023-09-01 08:52:54 +0530

    day-2

--- day1.js
+++ day1.js
@@ -1,6 +1,6 @@
 //Day1
 const http =require("http");
-const data=require('./data')
+const data=require('./day1-1')
 http.createServer((req,resp)=>{
   resp.writeHead(404,{'Content-Type':'application\json'});
   resp.write(JSON.stringify(data))
--- day2.js
+++ day2.js
@@ -1,14 +1,27 @@
 //Day2
 
+const { error } = require('console');
 const fs=require('fs');
-const input=process.argv;
+// const input=process.argv;
 
-if(input[2]=='add'){
-  fs.writeFileSync(input[3],input[4])
+// if(input[2]=='add'){
+//   fs.writeFileSync(input[3],input[4])
+// }
+// else if(input[2]=="remove"){
+//   fs.unlinkSync(input[3]);
+// }
+// else{
+//   console.log("invalid input")
+// }
+const path=require('path');
+const dirPath=path.join(__dirname,'files');
+for(let i=0;i<5;i++){
+    fs.unlinkSync(dirPath+"/prog"+i+".txt",'this is program file')
 }
-else if(input[2]=="remove"){
-  fs.unlinkSync(input[3]);
-}
-else{
-  console.log("invalid input")
-}
+fs.readdir(dirPath,(error,file)=>{
+    // console.log(file)
+    file.forEach((item)=>{
+        console.log(item)
+    })
+})
+// console.log(dirPath)

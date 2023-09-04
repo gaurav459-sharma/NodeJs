37a9360163b752343132b953c551ae454466b6a0
gaurav459-sharma <sharmag32145@gmail.comm>
2023-09-04 14:10:26 +0530

    render html file,dynamic routing and ejs

--- day5.js
+++ day5.js
@@ -5,12 +5,15 @@ const publicPath=path.join(__dirname,'public')
 
 // console.log(publicPath);
 // app.use(express.static(publicPath)); //we will not use this if we want to remove the extension from the url like .html
-//so for that we the previous approach as app.get("",(req,resp){}) in use sendfile in place of send
+//so for that we use the previous approach as app.get("",(req,resp){}) in use sendfile in place of send
+
 app.set("view engine","ejs");
 
 app.get("",(_,resp)=>{
   resp.sendFile(`${publicPath}/index.html`)
 })
+
+//example for making of dynamic page with the helps of ejs
 app.get("/profile",(_,resp)=>{
     const user={
         name:'Gaurav',
@@ -23,6 +26,8 @@ app.get("/profile",(_,resp)=>{
 app.get("/about",(_,resp)=>{
   resp.sendFile(`${publicPath}/about.html`)
 })
+
+//if there iswrong url then we can show 404 page by marking "*" in path
 app.get("*",(_,resp)=>{
   resp.sendFile(`${publicPath}/notFound.html`)
 })

7e139405b9f39ccf46a922ab482e8c08f5784101
gaurav459-sharma <sharmag32145@gmail.comm>
2023-11-02 23:47:57 +0530

    delete api

--- api.js
+++ api.js
@@ -1,13 +1,35 @@
 const express =require('express')
+const mongodb=require('mongodb')
 const dbConnect=require('./MongoConnect')
 
 const app=express();
+app.use(express.json());
 
 app.get('/',async(req,resp)=>{
     let data=await dbConnect();
     data=await data.find().toArray();
     // console.log(data)
     resp.send(data)
+})
+
+app.post('/',async(req,resp)=>{
+   let data=await dbConnect();
+   let result=await data.insertOne(req.body)
+    resp.send(result)
+})
+
+app.put('/:mobile',async(req,resp)=>{
+    // console.log(req.body)
+    let data=await dbConnect();
+    let result=await data.updateOne({mobile:req.params.mobile},{$set:req.body})
+    resp.send({result:"updated"})
+})
+
+app.delete('/:id',async(req,resp)=>{
+    console.log(req.params.id)
+    let data=await dbConnect();
+    let result=await data.deleteOne({_id:new mongodb.ObjectId(req.params.id)});
+    resp.send(result);
 })
 
 app.listen(5500)

f3f2cd6b74294d930cea65065bc85fb083041be8
gaurav459-sharma <sharmag32145@gmail.comm>
2023-11-05 23:00:16 +0530

    search api

--- index.js
+++ index.js
@@ -5,30 +5,64 @@ const Product=require('./product')
 const app=express();
 app.use(express.json())
 
-app.post('/create',async (req,resp)=>{
-    let data=new Product(req.body);
-    let result =await data.save();
-    console.log(req.body)
-    resp.send("done")
-})
+//search Api with multiple field
 
-app.get('/list',async(req,resp)=>{
-    let data=await Product.find();
-    resp.send(data);
-})
-app.delete('/delete/:_id',async(req,resp)=>{
-    console.log(req.params)
-    let data=await Product.deleteOne(req.params);
+app.get("/search/:key",async (req,resp)=>{
+    // resp.send("hello")
+    let data = await Product.find(
+        {
+            "$or":[
+                {"mobile":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}}
+            ]
+        }
+    )
     resp.send(data);
 })
 
-app.put('/update/:_id',async(req,resp)=>{
-    console.log(req.params)
-    let data=await Product.updateOne(
-        req.params,
-        {$set:req.body}
-    );
-    resp.send(data);
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.post('/create',async (req,resp)=>{
+//     let data=new Product(req.body);
+//     let result =await data.save();
+//     console.log(req.body)
+//     resp.send("done")
+// })
+
+// app.get('/list',async(req,resp)=>{
+//     let data=await Product.find();
+//     resp.send(data);
+// })
+// app.delete('/delete/:_id',async(req,resp)=>{
+//     console.log(req.params)
+//     let data=await Product.deleteOne(req.params);
+//     resp.send(data);
+// })
+
+// app.put('/update/:_id',async(req,resp)=>{
+//     console.log(req.params)
+//     let data=await Product.updateOne(
+//         req.params,
+//         {$set:req.body}
+//     );
+//     resp.send(data);
+// })
 
 app.listen(5000)
